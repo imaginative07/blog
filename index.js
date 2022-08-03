acf369d2a9cdfa35f1e57dcdb57d93a88b2dafb7
imaginative07 <66066294+imaginative07@users.noreply.github.com>
2022-08-04 01:45:02 +0530

    Basic express server setup on index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 const express = require('express');
+const dotenv = require('dotenv');
+const colors = require('colors');
+
+// Load Environment Variables
+dotenv.config({ path: './server/config/config.env' });
+
+const app = express();
+
+// Dev loggin middleware
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server is running on port ${PORT}`.yellow.bold));
+
+// Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise)=>{
+    console.log(`Error: ${err.message}`.red);
+    // Close server & exit process
+    server.close(()=> process.exit(1));
+});
